df23696b045ee893a1743f4c1c6458ed8cefdaa1
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-02-19 12:47:38 -0700

    added order randomization for answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 	// An Array to contain the questions from the question objects defined below
 	var questions = [];
-	
+
 	// 0th Question Object
 	var q0 = {
 		question: "What was the first video game released to the public?",
@@ -16,31 +16,55 @@ $(document).ready(function () {
 		a4: "Computer Space"
 	};
 	// Adding question to questions array
-	questions[questions.length] = q0.question;
+	questions[questions.length] = q0;
 
 	// 1st Question Object
 	var q1 = {
-		question:"Who is the main villain in Super Mario Bros.?",
+		question: "Who is the main villain in Super Mario Bros.?",
 		a1: "Ganon",
 		a2: "Wario",
 		a3: "Kamek",
 		a4: "Bowser"
 	};
 	// Adding question to questions array
-	questions[questions.length] = q1.question;
+	questions[questions.length] = q1;
 
-	
-	// How to randomly select a question from the array of questions
-	var randQuestion = questions[Math.floor(Math.random() * questions.length)];
+
+	// How to randomly select a question from the array of questions -- May eventually try to use the Knuth Shuffle to get a random question, too.
+	// First get the indictator of the random question
+	var randQuestionInd = questions[Math.floor(Math.random() * questions.length)];
+	// Then select the question property of that question object
+	var randQuestion = randQuestionInd.question;
 	var questionArea = '<div class="col-xs-12 blur"></div><div class="col-xs-10"></div><div class="col-xs-2"><h3 id="hearts"></h3></div><h2>Question: </h2><br><h3>' + randQuestion + '</h3>';
 	var fullHearts = 0;
 	var emptyHearts = 0;
-	// var answers = $(randQuestion).a1;
+	var answers = [randQuestionInd.a1, randQuestionInd.a2, randQuestionInd.a3, randQuestionInd.a4];
 	var answerArea = '<ul id="#answerArea"></ul>';
-	
+
+	// The Fisher-Yates (aka Knuth) Shuffle algorithm
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
+
+	var randAnswers = shuffle(answers);
 
 	// What happens when the start button is clicked:
-	function startBtn () {
+	function startBtn() {
 		console.log("Start button clicked!");
 		$("#startArea").html(questionArea);
 		for (let i = 0; i < fullHearts; i++) {
@@ -49,7 +73,7 @@ $(document).ready(function () {
 		for (let j = 0; j < emptyHearts; j++) {
 			$("#hearts").append('*');
 		}
-		
+		console.log(randAnswers);
 	}
 
 	// What happens when the game is reset
